fe1b7ed921123839f4eb8700450d9dea9a665fd4
Alice <alice.vergeau@gmail.com>
2024-01-11 20:59:31 -0500

    tp1 fini

--- tp1/tp1.cpp
+++ tp1/tp1.cpp
@@ -3,7 +3,11 @@
 #include <util/delay.h>
 
 bool pressed() {
-    return ((0b00000100 & PIND) == 0b00000100); 
+    if((0b00000100 & PIND) == 0b00000100) {
+        _delay_ms(15);
+        return ((0b00000100 & PIND) == 0b00000100);
+    }
+    return false; 
 }
 
 void couleur(int n) {
@@ -13,30 +17,29 @@ void couleur(int n) {
 int main() {
     DDRA = 0b00000011;
     DDRD = 0;
+    int i = 1;
     while(1) {
-        PORTA = 0;
-        int i = 1;
-        while(i <= 3) {
-            couleur(0);
-            if(pressed()) {
-                if(i > 2) {
-                    while(pressed()) {
-                        for(int i = 0; i < 40; i++) {
-                            couleur(1);
-                            _delay_ms(20);
-                            couleur(2);
-                            _delay_ms(50);
-                        } 
-                    }
-                    i++;
+        if(i > 3) {
+            PORTA = 0;
+            i = 1;
+        }
+        couleur(0);
+        if(pressed()) {
+            if(i > 2) {
+                while(pressed()) {
+                    couleur(1);
+                    _delay_ms(20);
+                    couleur(2);
+                    _delay_ms(30);
                 }
-                else {
-                    while(pressed()) {
-                        couleur(i);
-                        _delay_ms(2000);
-                    }
-                    i++;
+                i++;
+            }
+            else {
+                while(pressed()) {
+                    couleur(i);
+                    _delay_ms(50);
                 }
+                i++;
             }
         }
     }
