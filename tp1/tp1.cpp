465ac704298034e2342d651ef8f01bcebbaced7c
Alice <alice.vergeau@gmail.com>
2024-01-11 20:42:17 -0500

    for dans while dans if dans if dans wh9ile dans while

--- tp1/tp1.cpp
+++ tp1/tp1.cpp
@@ -1,18 +1,43 @@
 #include <avr/io.h>
-#define F_CPU 8000000
+#define F_CPU 800000
 #include <util/delay.h>
 
 bool pressed() {
-    return ((PIND & 0b00000100) == 0b00000100); 
+    return ((0b00000100 & PIND) == 0b00000100); 
+}
+
+void couleur(int n) {
+    PORTA = n;
 }
 
 int main() {
     DDRA = 0b00000011;
-    DDRD = 0x00;
-    while(pressed()) {
-        PORTA = 0b00000010;
-        _delay_ms(2000);
-        PORTA = 0b00000001;
-        _delay_ms(2000);
+    DDRD = 0;
+    while(1) {
+        PORTA = 0;
+        int i = 1;
+        while(i <= 3) {
+            couleur(0);
+            if(pressed()) {
+                if(i > 2) {
+                    while(pressed()) {
+                        for(int i = 0; i < 40; i++) {
+                            couleur(1);
+                            _delay_ms(20);
+                            couleur(2);
+                            _delay_ms(50);
+                        } 
+                    }
+                    i++;
+                }
+                else {
+                    while(pressed()) {
+                        couleur(i);
+                        _delay_ms(2000);
+                    }
+                    i++;
+                }
+            }
+        }
     }
 }
