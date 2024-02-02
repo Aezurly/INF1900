281c3d2bc79e0ee75b982e197432e060d3312625
Alice <alice.vergeau@gmail.com>
2024-02-01 19:18:46 -0500

    tp4 pb2

--- tp4/pb2/main.cpp
+++ tp4/pb2/main.cpp
@@ -33,35 +33,6 @@ const uint8_t BLINK_AMBER_DELAY = 10;
 volatile bool buttonPressed = false;
 volatile uint8_t gMinuterieExpiree; 
 
-ISR(INT0_vect) {
-    buttonPressed = !buttonPressed;
-    _delay_ms(30);
-
-    EIFR |= (1 << INTF0) ;
-}
-
-ISR('nidufer') {
-    gMinuterieExpiree = 1;
-}
-
-void initialisation ( void ) {
-    cli (); // désactive les interruptions (pause)
-
-    DDRA |= (1 << DDA0) | (1 << DDA1); // A0 et A1 en sortie
-    DDRD &= ~(1 << DDD2); // D2 en entrée
-
-    // cette procédure ajuste le registre EIMSK
-    // de l’ATmega324PA pour permettre les interruptions externes
-    EIMSK |= (1 << INT0) ; // active possibilité d'interuption sur INT0
-
-    // il faut sensibiliser les interruptions externes aux
-    // changements de niveau du bouton-poussoir
-    // en ajustant le registre EICRA
-    EICRA |= (1 << ISC00) ; // interruption sur les deux fronts
-
-    sei (); // active globablement les interruptions (play)
-}
-
 
 void setLedOff()
 {
@@ -89,9 +60,74 @@ void setLedAmber()
 	_delay_ms(BLINK_AMBER_DELAY);
 }
 
+
+ISR(INT0_vect) {
+    setLedGreen();
+    gMinuterieExpiree = 3;
+}
+
+void partirMinuterie (int duree) {
+    cli();
+    TCNT1 = 0 ; // valeur initiale du compteur
+
+    OCR1A = duree; // nb cycles pour 3s
+
+    TCCR1A |= 0 ; //normal mode
+    TCCR1B |= (1 << CS12) | (1 << CS10); // prescaler -> on utilise le timer 1 donc CS1 puis pour 1024 on a 101
+    TCCR1C |= 0;
+
+    TIMSK1 |= (1 << OCIE1A); // quand est-ce qu'on veut lancer l'interruption = aux temps A et B
+
+    sei();
+}
+
+ISR(TIMER1_COMPA_vect) {
+    switch(gMinuterieExpiree) {
+        case(0) : // au bout de 5s
+            partirMinuterie(39062);
+            gMinuterieExpiree++;
+            break;
+        
+        case(1) : // au bout de 10s
+            partirMinuterie(7812); // 1s
+            gMinuterieExpiree++;
+            setLedRed();
+            _delay_ms(100);
+            setLedOff();
+            DDRD &= ~(1 << DDD2); // D2 en entrée
+            break;
+        
+        case(2) : // au bout de 11s
+            DDRD |= (1 << DDD2); // D2 en sortie
+            setLedRed();
+            break;
+        case(3) :
+            break;
+    }
+}
+
+void initialisation ( ) {
+    cli (); // désactive les interruptions (pause)
+
+    DDRA |= (1 << DDA0) | (1 << DDA1); // A0 et A1 en sortie
+    DDRD |= (1 << DDD2); // D2 en sortie
+
+    // cette procédure ajuste le registre EIMSK
+    // de l’ATmega324PA pour permettre les interruptions externes
+    EIMSK |= (1 << INT0) ; // active possibilité d'interuption sur INT0
+
+    // il faut sensibiliser les interruptions externes aux
+    // changements de niveau du bouton-poussoir
+    // en ajustant le registre EICRA
+    EICRA |= (1 << ISC00) ; // interruption sur les deux fronts
+
+    sei (); // active globablement les interruptions (play)
+}
+
 int main()
 {
     initialisation();
+    partirMinuterie(39062);
 
     while (true)
     {
